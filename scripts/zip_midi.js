d5874d7c25697d43b367dd05aad3155a5a45fac0
Reuben Son <reubenson@gmail.com>
2023-11-30 17:31:54 -0500

    add scripts folder for hosting one-off script for creating a new zip file containing all midi assets

---
+++ scripts/zip_midi.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const archiver = require('archiver');
+
+let output = fs.createWriteStream('./src/assets/midi.zip');
+let archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+});
+
+// This event listener will tell you when the archive is finalized and the output file descriptor is closed.
+output.on('close', function() {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('Archiver has been finalized and the output file descriptor has closed.');
+});
+
+archive.pipe(output);
+
+// Append files from different directories
+directories = [
+    'src/assets/perso/midi/',
+    'src/assets/prairiefrontier/midi/',
+]
+for (let dir of directories)  {
+    archive.directory(dir, false)
+}
+
+// Finalize the archive (i.e., we are done appending files but streams have to finish yet)
+archive.finalize();
